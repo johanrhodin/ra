0081ddab43d24d0c50ade307df6134afe6bd91c5
Karl Nilsson <kjnilsson@gmail.com>
2022-10-12 15:52:24 +0100

    Add snapshot_bytes_written counter

--- src/ra.hrl
+++ src/ra.hrl
@@ -220,6 +220,8 @@
           "Total number of snapshots written"},
          {snapshot_installed, ?C_RA_LOG_SNAPSHOTS_INSTALLED, counter,
           "Total number of snapshots installed"},
+         {snapshot_bytes_written, ?C_RA_LOG_SNAPSHOT_BYTES_WRITTEN, counter,
+          "Number of snapshot bytes written (not installed)"},
          {reserved_1, ?C_RA_LOG_RESERVED, counter, "Reserved counter"}
          ]).
 -define(C_RA_LOG_WRITE_OPS, 1).
@@ -232,7 +234,8 @@
 -define(C_RA_LOG_FETCH_TERM, 8).
 -define(C_RA_LOG_SNAPSHOTS_WRITTEN, 9).
 -define(C_RA_LOG_SNAPSHOTS_INSTALLED, 10).
--define(C_RA_LOG_RESERVED, 11).
+-define(C_RA_LOG_SNAPSHOT_BYTES_WRITTEN, 11).
+-define(C_RA_LOG_RESERVED, 12).
 
 -define(C_RA_SRV_AER_RECEIVED_FOLLOWER, ?C_RA_LOG_RESERVED + 1).
 -define(C_RA_SRV_AER_REPLIES_SUCCESS, ?C_RA_LOG_RESERVED + 2).
